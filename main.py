c984103f063e5912bcb5e1e8aadcbcddae18c07d
Anton <yuschkof.toscha@yandex.ru>
2022-12-03 10:16:26 +0400

    работает?

--- main.py
+++ main.py
@@ -8,27 +8,31 @@ st.write("Описание из Википедии [RC5](https://ru.wikipedia.or
 
 with st.sidebar:
     bite = st.selectbox("Длина слова в битах: ", (16, 32, 64))
-    number = st.number_input('Количество раундов:', 1, 255)
+    number = st.number_input('Количество раундов:', min_value=1, max_value=255)
     keys = st.text_input('Ключ', 'Key')
 
 
-col1, col2 = st.columns(2)
+tab1, tab2 = st.tabs(["Зашифровать", "Расшифровать"])
 
-with col1:
-    st.subheader('Зашифровать слово')
+with tab1:
     string = ""
     text = st.text_input('Слово', 'Hello world')
     if st.button('Зашифровать'):
         try:
             rc5 = RC5(keys.encode(), bite, number)
             encryptText = rc5.encrypt(text.encode('utf-8'))
-            string += "{}".format(b''.join(encryptText).hex())
-            st.markdown(f"Зашифрованное слово: {string}")
+            t = [text[i:i + rc5.blockSize * 2] for i in range(0, len(text), rc5.blockSize * 2)]
+            for i, j in zip(t, encryptText):
+                if len(i) < (rc5.blockSize * 2):
+                    i += " " * (rc5.blockSize * 2 - len(i))
+                string = "{}\t--> {}\n".format(i, j.hex())
+                st.markdown(f"{string}")
+            string += ">>\n>> encrypt: {}\n".format(b''.join(encryptText).hex())
+            st.markdown(f"{text} --> {'{}'.format(b''.join(encryptText).hex())}")
         except ValueError:
             st.markdown(f"Ошибка, попробуйте снова")
 
-with col2:
-    st.subheader('Расшифровать слово')
+with tab2:
     string = ""
     text = st.text_input('Зашифрованное слово', '')
     if st.button('Расшифровать'):
@@ -36,8 +40,14 @@ with col2:
             rc5 = RC5(keys.encode(), bite, number)
             decryptText = rc5.decrypt(bytes.fromhex(text))
             utf8String = str(b''.join(decryptText), 'utf-8')
-            string += "{}\n".format(utf8String)
-            st.markdown(f"Расшифрованное слово: {string}")
+            b = [text[i:i + rc5.blockSize * 4] for i in range(0, len(text), rc5.blockSize * 2)]
+            t = [utf8String[i:i + rc5.blockSize * 2] for i in range(0, len(utf8String), rc5.blockSize * 2)]
+            for i, j in zip(b, t):
+                if len(i) < (rc5.blockSize * 2):
+                    i += " " * (rc5.blockSize * 2 - len(i))
+                string = "{}\t--> {}\n".format(i, j)
+                st.markdown(f"{string}")
+            st.markdown(f"{text} --> {utf8String}")
         except ValueError:
             st.markdown(f"Ошибка, попробуйте снова")
 
